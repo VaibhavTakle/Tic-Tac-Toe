f8f81870eb18d25aa62ca4e59ca581de63e77ff8
Vaibhav Takle <vaibhavdtakle@gmail.com>
2021-02-06 22:04:45 +0530

    Deploying tic-tac-toe app

---
+++ src/Game.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Board from "./Board";
+import "./index.css";
+class Game extends Component {
+  render() {
+    return (
+      <div className="Game">
+        <Board />
+      </div>
+    );
+  }
+}
+
+export default Game;
